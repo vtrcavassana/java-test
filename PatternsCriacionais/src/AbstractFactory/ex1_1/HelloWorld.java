37f6530849605607edf21e63bc06e800ed4c96b8
vtrcavassana <victor.cavassana@outlook.com>
2020-06-05 23:17:34 -0300

    1

--- PatternsCriacionais/src/AbstractFactory/ex1_1/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-package AbstractFactory.ex1_1;
-
-public interface HelloWorld {
-
-	public String print();
-}
--- PatternsCriacionais/src/AbstractFactory/ex1_1/HelloWorldFactory.java
+++
@@ -1,7 +0,0 @@
-package AbstractFactory.ex1_1;
-
-public interface HelloWorldFactory {
-	
-	public HelloWorld getInstance();
-
-}
